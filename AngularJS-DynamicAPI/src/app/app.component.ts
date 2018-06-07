029097ca607fa3fa74994393c1d0c4b8e353aa6e
Archana Miryala <archanmiryal>
2018-06-07 00:19:51 -0400

    A

--- AngularJS-DynamicAPI/src/app/app.component.ts
+++ AngularJS-DynamicAPI/src/app/app.component.ts
@@ -4,9 +4,21 @@ import {Component} from '@angular/core';
 
     selector:'app-comp',
     template:`<div>
-                   <h1>This is Angular JS</h1>
-                   <prod-comp></prod-comp>
-              </div>`
+              <nav class="navbar navbar-inverse">
+                 <div class="container-fluid">
+                    <a class="navbar-brand">Online Shopping Cart</a>
+                         <ul class="nav navbar-nav">
+                         <li><a [routerLink]="['/home']">Home</a></li>
+                         <li><a [routerLink]="['/products']">Products</a></li>
+                         <li><a [routerLink]="['/orders ']">Orders</a></li>
+                         </ul>
+
+                  </div>
+
+                </nav>
+                  
+              </div>
+              <router-outlet></router-outlet>`
 })
 
 export class AppComponent
--- AngularJS-DynamicAPI/src/app/app.module.ts
+++ AngularJS-DynamicAPI/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { EmpComponent } from './emp.component';
 import { StudentComponent } from './Std.component';
@@ -11,7 +14,11 @@ import { ProductFilterPipe } from './products/product-filter.component';
 import { ProductFilterCodePipe } from './products/product-filtercode.component';
 import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
-import { HttpModule } from '@angular/http';
+
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './orders/order.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { NotFoundComponent } from './notfound/notfound.component';
 
 
 
@@ -20,7 +27,15 @@ import { HttpModule } from '@angular/http';
     imports:[
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot([
+            {path:'products', component:ProductComponent},
+            {path:'products/:id', component:ProductDetailComponent},
+            {path:'orders', component:OrderComponent},
+            {path:'home',component:HomeComponent},
+            {path:'', redirectTo:'home', pathMatch:'full'},
+            {path:'**', component:NotFoundComponent}
+        ])
     ],
     //All componenet and Pipes declare here
     declarations:[
@@ -32,7 +47,11 @@ import { HttpModule } from '@angular/http';
         ProducutAddValue,
         ProductFilterPipe,
         ProductFilterCodePipe,
-        StarComponent
+        StarComponent,
+        HomeComponent,
+        OrderComponent,
+        ProductDetailComponent,
+        NotFoundComponent
     ],
     //One main componenet come here
     bootstrap:[
